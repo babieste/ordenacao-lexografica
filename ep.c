cccee62e0ef1755a3faf1b1c69ee8e274321a6da
Bárbara Este <barbaraeste01@gmail.com>
2019-09-29 16:34:04 -0300

    improvemens to insertion sort; not finished

--- ep.c
+++ ep.c
@@ -6,33 +6,60 @@
 #define BUFFER_SIZE 50
 
 void insertionSort(FILE *in, FILE *out) {
-	fpos_t *posOut;
-	int i = 0, ascii;
+	int i = 0, j = 0, posOut, flag = 0;
 	char bufferIn[BUFFER_SIZE], bufferOut[BUFFER_SIZE], cIn, cOut;
 
 	while(1) {
 		cIn = fgetc(in);
-		if(cIn == EOF) break;
-		if(cIn == ' ' || cIn == '\0') {
-			bufferIn[i] = ' ';
-			
+		if(cIn == ' ' || cIn == '\0' || cIn == EOF) {
 			fseek(out, 0, SEEK_SET);
 			cOut = fgetc(out);
 			
-			//se c == EOF, significa que nao ha palavras em out, portanto passamos
-			//bufferIn para out diretamente, sem validacao
 			if(cOut != EOF) {
-				//verificando valor ascii
-				// se a palavra lida eh, alfabeticamente falando, menor que a que foi lida 
-				//do arquivo out, entao colocar antes.
-				if ((int) bufferIn[0] < (int) cOut) {
-					fseek(out, 0, SEEK_SET);
-				} else {
-					fseek(out, 0, SEEK_END);
+				//se cOut == EOF, significa que nao ha palavras em out, portanto passamos
+				//bufferIn para out diretamente, sem validacao
+				
+				//salvando ponteiro para primeiro char de cOut
+				posOut = ftell(out);
+				
+				//voltar o pointeiro para trás para salvar a palavra toda no laco
+				fseek(out, -1, SEEK_CUR);
+				
+				while(1) {
+					cOut = fgetc(out);
+					if(cOut == ' ' || cOut == '\0' || cOut == EOF) {
+						//termino da leitura da palavra atual
+
+						j = 0;
+						while(1) {
+							//comparando bufferIn e bufferOut por valores ascii
+							if((int) bufferIn[j] == (int) bufferOut[j]) {
+								//se os caracteres atuais de bufferIn e bufferOut sao iguais
+								//devemos entao ler os proximos
+								j++;
+							} else {
+								if((int) bufferIn[j] < (int) bufferOut[j]) {
+									//se a palavra em bufferIn for, alfabeticamente falando,
+									//menor do que a palavra em bufferOut, entao ela vem antes
+									flag = 1;
+									
+									//coloca o pointeiro de out no inicio da palavra lida em
+									//bufferOut para gravar a palavra em bufferIn antes dela
+									fseek(out, 0, posOut);
+								}
+								break;
+							}
+						}
+						
+						if(flag) break;
+						if(cOut == EOF) break;
+						memset(bufferOut, 0, BUFFER_SIZE);
+						j = 0;
+					}
+					bufferOut[j] = cOut;
+					j++;
 				}
 			}
-			
-			printf("\nPalavra: %s - caracter: %c (ascii: %d)\nLeitura do buffer: %c (ascii: %d)", bufferIn, bufferIn[0], (int) bufferIn[0], cOut, (int) cOut);
 			
 			if(fputs(bufferIn, out) != EOF) {
 				if (fputc(' ', out) == EOF) {
@@ -43,6 +70,8 @@ void insertionSort(FILE *in, FILE *out) {
 				printf("\nErro ao gravar palavra em \"out1.txt\".");
 				break;
 			}
+			
+			if(cIn == EOF) break;
 			memset(bufferIn, 0, BUFFER_SIZE);
 			i = 0;
 		} else {
@@ -53,10 +82,7 @@ void insertionSort(FILE *in, FILE *out) {
 }
 
 int main() {
-	FILE *in = fopen("in.txt", "r"), *out1 = fopen("out1.txt", "r+"), *out2;
-	fpos_t *posIn;
-	int tamanho;
-	char palavra[BUFFER_SIZE], c;
+	FILE *in = fopen("in.txt", "r"), *out1 = fopen("out1.txt", "w+"), *out2;
 	
 	if(in == NULL || out1 == NULL) {
 		printf("Erro ao abrir um ou mais arquivos.\n");
